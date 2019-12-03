be0cba073899c8bdc672af64f0add8182203cb13
junejoo <9491086@gmail.com>
2019-12-03 16:02:35 +0900

    Update Main.java

--- se/Main.java
+++ se/Main.java
@@ -2,7 +2,7 @@ package se;
 
 // 1: remote by junejoo
 // 1: local by junejoo (push)
-// 1: remote by junejoo (fetch)
+// 1: remote by junejoo (fetch, pull)
 public class Main {
 
 	public static void main(String[] args) {
