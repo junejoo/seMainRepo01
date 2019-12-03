8dfd0ff172d67901426b9d48e2ac65b421f77c28
junejoo <9491086@gmail.com>
2019-12-03 15:55:25 +0900

    Update Main.java
    add local comment

--- se/Main.java
+++ se/Main.java
@@ -1,10 +1,10 @@
 package se;
 
 // 1: remote by junejoo
+// 1: local by junejoo
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 	}
 }
